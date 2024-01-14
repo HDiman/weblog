2fd7ea3c957042bb2970380072c92230ca344517
Dmitry Sannikov <HDiman@users.noreply.github.com>
2024-01-15 00:29:39 +0300

    Rework models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-# Создание модели
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 
 class Post(models.Model):
@@ -23,6 +26,8 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+    objects = models.Manager()  # менеджер, применяемый по умолчанию
+    published = PublishedManager()  # конкретно-прикладной менеджер
 
     class Meta:
         ordering = ['-publish']  # убывающий порядок по полю "publish"
