182c3afc518d504a112286d097d6192486ce8f60
Dmitry Sannikov <HDiman@users.noreply.github.com>
2024-01-16 22:33:58 +0300

    Work with models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -15,17 +16,13 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'  # опубликовано
 
     title = models.CharField(max_length=250)  # название
-    slug = models.SlugField(max_length=250)  # slug исп. для поиска
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='blog_posts')  # пользователь
+    slug = models.SlugField(max_length=250, unique_for_date='publish')  # уникальный slug исп. для поиска
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')  # пользователь
     body = models.TextField()  # текст
     publish = models.DateTimeField(default=timezone.now)  # дата публикации поста
     created = models.DateTimeField(auto_now_add=True)  # дата создания поста
     updated = models.DateTimeField(auto_now=True)  # дата обновления поста
-    status = models.CharField(max_length=2,
-                              choices=Status.choices,
-                              default=Status.DRAFT)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
     objects = models.Manager()  # менеджер, применяемый по умолчанию
     published = PublishedManager()  # конкретно-прикладной менеджер
 
@@ -37,3 +34,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
